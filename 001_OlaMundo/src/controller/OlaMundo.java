73037260e0b16a89b171dc78a179ce9460b03e7a
Carlos Daniel Freitas de Souza <44007817+CarlosDaniel19@users.noreply.github.com>
2018-10-18 21:10:42 -0300

    Add files via upload

---
+++ 001_OlaMundo/src/controller/OlaMundo.java
@@ -0,0 +1,14 @@
+package controller;
+
+public class OlaMundo {
+	
+	/*
+	 * This is your first JAVA program
+	 */
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+		System.out.println(10);
+
+	}
+		
+}
